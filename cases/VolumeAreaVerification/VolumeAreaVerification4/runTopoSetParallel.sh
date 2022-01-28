a3c61545ebaf208e329b1e28ce504380455b00df
Vozze42 <servaas.clerckx@gmail.com>
2022-01-28 22:59:07 +0100

    clean VolumeArea

--- cases/VolumeAreaVerification/VolumeAreaVerification4/runTopoSetParallel.sh
+++
@@ -1,7 +0,0 @@
-. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions #Source runfunctions
-echo "decomposePar"
-runApplication decomposePar
-echo "topoSet"
-runParallel topoSet
-echo "reconstructParMesh"
-runApplication reconstructParMesh -constant
